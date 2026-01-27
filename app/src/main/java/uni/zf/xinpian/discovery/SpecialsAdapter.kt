a19e7f4dffffd3b2fdfab5c4edf7e669da0b250e
zf <zhufengweihai@gmail.com>
2026-01-27 23:18:07 +0800

    1.5.0

--- app/src/main/java/uni/zf/xinpian/discovery/SpecialsAdapter.kt
+++ app/src/main/java/uni/zf/xinpian/discovery/SpecialsAdapter.kt
@@ -1,11 +1,15 @@
 package uni.zf.xinpian.discovery
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import uni.zf.xinpian.category.TagVideoListActivity
+import uni.zf.xinpian.data.AppConst.ARG_DY_TAG
+import uni.zf.xinpian.data.AppConst.ARG_TAG_TITLE
 import uni.zf.xinpian.databinding.ItemSpecialBinding
 import uni.zf.xinpian.json.model.Special
 import uni.zf.xinpian.utils.ImageLoadUtil.loadImages
@@ -31,10 +35,18 @@ class SpecialsAdapter() : PagingDataAdapter<Special, SpecialsAdapter.ViewHolder>
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        getItem(position)?.let {
+        getItem(position)?.let { special ->
             val binding = holder.binding
-            binding.tvTitle.text = it.title
-            loadImages(binding.ivCover, it.coverUrl)
+            binding.tvTitle.text = special.title
+            loadImages(binding.ivCover, special.coverUrl)
+            binding.root.setOnClickListener {
+                val context = binding.root.context
+                context.startActivity(
+                Intent(context, TagVideoListActivity::class.java).apply {
+                    putExtra(ARG_DY_TAG, special.topicId)
+                    putExtra(ARG_TAG_TITLE, special.title)
+                })
+            }
         }
     }
 }
